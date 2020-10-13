a34662853ac0481ec14a2f4b6ec07835bbaa22da
Bruno Rocha <brunorocha.ti2010@gmail.com>
2020-10-13 20:16:37 -0300

    ajuste telas de login

--- AppFilmes/ViewController.swift
+++ AppFilmes/ViewController.swift
@@ -8,15 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var txtLogin: UITextField!
+    
+    @IBOutlet var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("teste")
+        
     }
     
     
+    @IBAction func openAction(_ sender: Any) {
+        
+        if (txtLogin.text == "bruno" && txtPassword.text == "1234" ) {
+            performSegue(withIdentifier: "tela2", sender: self)
+            
+        }
+        else {
+        print("náo deu certo")
+        }
+    
+    }
 
+    
 }
+
 
